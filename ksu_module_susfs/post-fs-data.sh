03962b237b09a1915ee07dd7132a27d1bf91c015
omajili-manbu <qwer12777332899@gmail.com>
2026-08-21 05:42:39 +0800

    module: enable susfs avc log spoofing by default in post-fs-data.sh
    The 'enable_avc_log_spoofing 1' command was previously wrapped in a
    'cat <<EOF >/dev/null' block so it never ran. Enable it by default using
    ${SUSFS_BIN} so SELinux AVC audit lines from app processes are sanitized
    (see kernel_patches/55_selinux_hook_hide_susfs_artifacts.patch).

--- ksu_module_susfs/post-fs-data.sh
+++ ksu_module_susfs/post-fs-data.sh
@@ -97,11 +97,9 @@ ${SUSFS_BIN} add_sus_kstat_statically '/proc/fs/jbd2' 'default' 'default' '2' 'd
 EOF
 
 #### Enable avc log spoofing to bypass 'su' domain detection via /proc/<pid> enumeration, effective for all processes ####
-cat <<EOF >/dev/null
-ksu_susfs enable_avc_log_spoofing 1
+${SUSFS_BIN} enable_avc_log_spoofing 1
 ## disable it when users want to do some debugging with the permission issue or selinux issue ##
-#ksu_susfs enable_avc_log_spoofing 0
-EOF
+## ${SUSFS_BIN} enable_avc_log_spoofing 0
 
 #### Hide all sus mounts for NON-SU processes in this stage just to prevent zygote from caching them in memory ####
 ## This should be mainly applied if you have ReZygisk enabled but without TreatWheel module ##
